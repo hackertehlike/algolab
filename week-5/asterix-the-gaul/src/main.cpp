eb334534efa6e3765b3e5ee49f56956707bc07ce
hackertehlike <59940605+hackertehlike@users.noreply.github.com>
2024-11-01 14:27:13 +0100

    Knights and Coin Tossing

---
+++ week-5/asterix-the-gaul/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+void testcase() {
+    
+}
+
+int main() {
+
+    std::ios_base::sync_with_stdio(false);
+
+    int t; std::cin >> t;
+
+    while(t--) {
+        testcase();
+    }
+
+    return 0;
+}
